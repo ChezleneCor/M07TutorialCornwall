8db7d14c6e8c04495a5cc3ec0932c3d229cd7dd4
Chezlene <chezlene.cor06@gmail.com>
2024-05-03 23:09:59 -0400

    completed tutorial

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const authRoutes = require('./routes/authRoutes');
 const app = express();
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://chez:Password123@nodetuts.kzvqhwx.mongodb.net/m07Tutorial?retryWrites=true&w=majority&appName=nodetuts';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
